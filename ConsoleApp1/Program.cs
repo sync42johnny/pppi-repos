eeb443320c321182b1455ffadbb2983c642ae0c5
Ivan Boretskyi <boretskiy7.open1@gmail.com>
2023-02-25 17:33:05 +0200

    method GetRatingAsync

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,36 +1,59 @@
 ﻿using ConsoleApp1;
-using System.Threading;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string originalText = "I'm so happy!";
-        string[] wordsToReplace = { "happy" };
-        string[] emojis = { "😊" };
+        //string originalText = "I'm so happy!";
+        //string[] wordsToReplace = { "happy" };
+        //string[] emojis = { "😊" };
 
-        Thread t1 = new Thread(() => EmojiReplacer.ReplaceWordsWithEmojis(originalText, wordsToReplace, emojis));
-        Thread t2 = new Thread(new ThreadStart(Printer.PrintGreetings));
-        Thread t3 = new Thread(new ThreadStart(NumberProcessor.PrintOddAndEvenNumbers));
+        //Thread t1 = new Thread(() => EmojiReplacer.ReplaceWordsWithEmojis(originalText, wordsToReplace, emojis));
+        //Thread t2 = new Thread(new ThreadStart(Printer.PrintGreetings));
+        //Thread t3 = new Thread(new ThreadStart(NumberProcessor.PrintOddAndEvenNumbers));
 
-        t1.Start();
-        t2.Start();
-        t3.Start();
+        //t1.Start();
+        //t2.Start();
+        //t3.Start();
 
-        t1.Join();
-        t2.Join();
-        t3.Join();
+        //t1.Join();
+        //t2.Join();
+        //t3.Join();
 
 
         Console.WriteLine("Starting asynchronous methods...");
 
         // Call asynchronous methods
+
+        // GetRatingAsync
+        Task<int> ratingTask = Task.Run(() => GetRatingAsync("Naruto"));
+
+        Console.WriteLine($"Retrieving user rating for Naruto...");
+
+        int rating = ratingTask.Result;
+
+        Console.WriteLine($"The average user rating for Naruto is {rating}%.");
+
+        // DownloadFileAsync
         Task.Run(() => DownloadFileAsync("https://cdn.myanimelist.net/images/anime/2/75259.jpg", "D:/Downloads/output.jpg")).Wait();
+        
+        // PrintFizzBuzz
         Task.Run(() => PrintFizzBuzz(50)).Wait();
 
         Console.WriteLine("All asynchronous methods have completed.");
 
         Console.ReadLine();
+    }
+
+    static async Task<int> GetRatingAsync(string animeName)
+    {
+        // We'll simulate a delay to simulate the time it take
+        // to retrieve data from a database or other data source.
+        await Task.Delay(1000);
+
+        // simulate the user rating for the anime.
+        Random rand = new Random();
+        return rand.Next(50, 101);
     }
 
     static async Task DownloadFileAsync(string url, string outputFilename)
